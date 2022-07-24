12b15809b33e3993d82bd77ce8ccdd99b998b55a
Junichi Ito <jit@sonicgarden.jp>
2022-07-24 10:53:40 +0900

    Add seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Article.delete_all
+
+30.times do |n|
+  Article.create!(title: "Title #{n}", body: "Body #{n}")
+end
